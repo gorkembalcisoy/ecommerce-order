77a0734aeb28c21eae5dc21cf7a17fa4c5c4c579
Gorkem Balcisoy <gbalcisoy@gmail.com>
2024-09-14 19:50:23 +0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ecommerce-order
 E-Commerce Order Service
+
+http://localhost:8081/swagger-ui/index.html
---
+++ src/test/java/com/ecommerce/order/OrderApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ecommerce.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
